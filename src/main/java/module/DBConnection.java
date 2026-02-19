2421ee114ecedfa2e32c3dfc9da8866e4a2807ec
Amboara <amboaraniainarkt@gmail.com>
2026-02-19 16:21:35 +0300

    first commit

---
+++ src/main/java/module/DBConnection.java
@@ -0,0 +1,17 @@
+package module;
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+
+public class DBConnection {
+    public static Connection getConnection() {
+        try {
+            String jdbcURl = System.getenv("jdbc:postgresql://localhost:5432/annex_db");
+            String user = System.getenv("postgres");
+            String password = System.getenv("123");
+            return DriverManager.getConnection(jdbcURl, user, password);
+        } catch (SQLException e) {
+            throw new RuntimeException(e);
+        }
+    }
+}
